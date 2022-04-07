7169797d3214f19dee4e9c43f4a066fddd0f4cb7
Antonio Teixeira <antonio.teixeira@blinctek.com>
2022-04-07 18:40:32 -0400

    fix: change map to list

--- bin/primeiro_projeto.dart
+++ bin/primeiro_projeto.dart
@@ -1,23 +1,23 @@
 enum Status { none, circle, cross }
 enum Player { circle, cross }
 
-Map<int, Status> board = {
-  1: Status.none,
-  2: Status.none,
-  3: Status.none,
-  4: Status.none,
-  5: Status.none,
-  6: Status.none,
-  7: Status.none,
-  8: Status.none,
-  9: Status.none,
-};
+List<Status> board = [
+  Status.none,
+  Status.none,
+  Status.none,
+  Status.none,
+  Status.none,
+  Status.none,
+  Status.none,
+  Status.none,
+  Status.none,
+];
 
 Player turn = Player.cross;
 
 void main(List<String> arguments) {
   // Initializing board
-  board.forEach((key, value) => {
-        print({key, value})
-      });
+  for (var status in board) {
+    print(status);
+  }
 }
